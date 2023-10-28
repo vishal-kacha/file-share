25ce12bdc3a36da47c2a42d800a5bf33561e7ce6
Kachavi <vishaldineshbhaikacha@gmail.com>
2023-10-28 20:29:40 +0530

    test

---
+++ app/layout.jsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-black text-white">{children}</body>
+    </html>
+  );
+}
