1a08648a103994031a364f382ffc13434a1f58a2
Kachavi <vishaldineshbhaikacha@gmail.com>
2023-10-29 09:42:37 +0530

    added styles

--- api/upload/file.js
+++ api/upload/file.js
@@ -7,6 +7,7 @@ export async function POST(request) {
 
   const blob = await put(filename, request.body, {
     access: "public",
+    addRandomSuffix: false,
   });
 
   return NextResponse.json(blob);
--- app/page.jsx
+++ app/page.jsx
@@ -5,38 +5,89 @@ import { useState, useRef } from "react";
 export default function AvatarUploadPage() {
   const inputFileRef = useRef(null);
   const [blob, setBlob] = useState(null);
+
+  function copytext() {
+    const text = document.getElementById("urltext");
+    navigator.clipboard.writeText(text.innerText);
+    const button = document.getElementById("copybutton");
+    button.innerText = "✔";
+    setTimeout(function () {
+      button.innerText = "📋";
+    }, 2000);
+  }
+
   return (
     <>
-      <h1>Upload Your Avatar</h1>
+      <div className="flex flex-col items-center justify-center h-screen w-screen gap-4">
+        <h1 className="text-4xl font-semibold">Upload Your file</h1>
 
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
+        <form
+          className="flex flex-col justify-center bg-neutral-800 bg-opacity-80 p-12 rounded-lg gap-4"
+          onSubmit={async (e) => {
+            e.preventDefault();
 
-          const file = inputFileRef.current.files[0];
+            const file = inputFileRef.current.files[0];
 
-          console.log(file);
-          const response = await fetch(
-            `/api/upload/file?filename=${file.name}`,
-            {
-              method: "POST",
-              body: file,
-            }
-          );
+            console.log(file);
+            const response = await fetch(
+              `/api/upload/file?filename=${file.name}`,
+              {
+                method: "POST",
+                body: file,
+              }
+            );
 
-          const newBlob = await response.json();
-          console.log(newBlob);
-          setBlob(newBlob);
-        }}
-      >
-        <input name="file" ref={inputFileRef} type="file" required />
-        <button type="submit">Upload</button>
-      </form>
-      {blob && (
-        <div>
-          Blob url: <a href={blob.url}>{blob.url}</a>
-        </div>
-      )}
+            const newBlob = await response.json();
+            console.log(newBlob);
+            setBlob(newBlob);
+          }}
+        >
+          {/* <input name="file" ref={inputFileRef} type="file" required />
+          <button type="submit">Upload</button>
+        </form>
+        {blob && (
+          <div>
+            Blob url: <a href={blob.url}>{blob.url}</a>
+          </div>
+        )} */}
+          <label htmlFor="Upload" className="text-xl">
+            Select file to Upload
+          </label>
+          <input
+            type="file"
+            ref={inputFileRef}
+            name="Upload"
+            id="Upload"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-white text-black rounded-md text-xl font-medium p-1"
+          >
+            Upload
+          </button>
+        </form>
+        {blob && (
+          <div>
+            <h1 className="text-2xl p-2 text-center">Your URL</h1>
+            <div className="bg-neutral-800 p-2 rounded-md">
+              <p id="urltext" className="inline p-2">
+                {blob.url}
+              </p>
+              <button
+                id="copybutton"
+                className="bg-black p-2 m-2 rounded-md bg-opacity-40"
+                onClick={(e) => {
+                  e.preventDefault();
+                  copytext();
+                }}
+              >
+                📋
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
